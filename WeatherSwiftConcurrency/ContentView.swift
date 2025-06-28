51918313ce0170fd3e99e13c6a5b421b856728cd
仲優樹 <yu-ki216m@outlook.jp>
2025-06-28 21:00:43 +0900

    Initial Commit

---
+++ WeatherSwiftConcurrency/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherSwiftConcurrency
+//
+//  Created by 仲優樹 on 2025/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherSwiftConcurrency/WeatherSwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftConcurrencyApp.swift
+//  WeatherSwiftConcurrency
+//
+//  Created by 仲優樹 on 2025/06/28.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
